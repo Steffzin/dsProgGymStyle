7231a624b3bc25c767f009e94415c1bcfd4cb5a5
Lucas-Phelipe <lucasphelipe280@gmail.com>
2025-03-30 21:50:08 -0300

    log icon user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -23,10 +23,14 @@ router.post('/login', (req, res) => {
 
     const user = database.users.find((user) => user.email === email && user.password === password);
     if (!user) {
-        return res.status(400).json({ message: 'Email ou senha incorretos!' });
+        return res.status(404).json({ message: 'Usuário não existente, crie uma conta!' });
     }
 
-    res.status(200).json({ message: 'Login bem-sucedido!', userId: user.id });
+    if (user.password !== password) {
+        return res.status(400).json({ message: 'Senha incorreta!' });
+    }
+
+    res.status(200).json({ message: 'Login bem-sucedido!', userId: user.id, username: user.name});
 });
 
 module.exports = router;
